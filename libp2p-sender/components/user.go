327eb4ba6dba33da20229982272c54d7d042bb1e
samer955 <samer.osman95@hotmail.com>
2022-08-08 14:32:47 +0200

    update .env

---
+++ libp2p-sender/components/user.go
@@ -0,0 +1,59 @@
+package components
+
+import (
+	"bufio"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+func GetNumberOfOnlineUser() int {
+	if runtime.GOOS == "linux" {
+		output, _ := exec.Command("who").Output()
+		return outputToIntLinux(string(output))
+	}
+	if runtime.GOOS == "windows" {
+		output, _ := exec.Command("query", "user").Output()
+		return outputToIntWindows(string(output))
+	}
+	return 0
+}
+
+func outputToIntWindows(output string) int {
+	var users = 0
+
+	scanner := bufio.NewScanner(strings.NewReader(output))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		words := strings.Fields(line)
+		if strings.HasPrefix(words[3], "Active") {
+			users++
+		}
+	}
+	err := scanner.Err()
+	if err != nil {
+		return 0
+	}
+	return users
+}
+
+func outputToIntLinux(output string) int {
+	var users = 0
+
+	scanner := bufio.NewScanner(strings.NewReader(output))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		users++
+	}
+	err := scanner.Err()
+	if err != nil {
+		return 0
+	}
+	return users
+}
