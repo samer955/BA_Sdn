017d4c059ca6fa09a03d2cc39745f6e07859ed67
samer955 <samer.osman95@hotmail.com>
2022-09-08 13:41:04 +0200

    discovery tests added db-agent

--- db-agent/discovery/discovery.go
+++ db-agent/discovery/discovery.go
@@ -2,20 +2,21 @@ package discovery
 
 import (
 	"context"
+	"db-agent/node"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
 	"log"
-	"time"
 )
-
-var PeerList []peer.AddrInfo
 
 type discoveryNotifee struct {
 	node host.Host
 }
 
+var PeerList []peer.AddrInfo
+
 func (d *discoveryNotifee) HandlePeerFound(info peer.AddrInfo) {
+
 	if d.node.ID().Pretty() != info.ID.Pretty() {
 		log.Printf("discovered a new peer %s\n", info.ID.Pretty())
 
@@ -27,16 +28,13 @@ func (d *discoveryNotifee) HandlePeerFound(info peer.AddrInfo) {
 		PeerList = append(PeerList, info)
 		log.Printf("connected to Peer %s ", info.ID.Pretty())
 	}
+
 }
 
-func SetupDiscovery(node host.Host, discoveryName string) error {
-	discovery := mdns.NewMdnsService(node, discoveryName, &discoveryNotifee{node: node})
-	start := discovery.Start()
+func SetupDiscovery(node node.Node, discoveryName string) error {
 
-	//If any error is returned try again in 1min
-	if start != nil {
-		time.Sleep(60 * time.Second)
-		SetupDiscovery(node, "")
-	}
+	discovery := mdns.NewMdnsService(node.Host, discoveryName, &discoveryNotifee{node: node.Host})
+	start := discovery.Start()
 	return start
+
 }
---
+++ db-agent/discovery/discovery_test.go
@@ -0,0 +1,61 @@
+package discovery
+
+import (
+	"db-agent/node"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestSetupDiscovery(t *testing.T) {
+	host, _ := libp2p.New()
+	var node = node.Node{Host: host}
+	t.Cleanup(func() {
+		node.Host.Close()
+	})
+
+	err := SetupDiscovery(node, "discoveryDB_Test")
+
+	assert.Nil(t, err)
+}
+
+func secondPeer(t *testing.T, discoveryName string) host.Host {
+	node, _ := libp2p.New()
+	discovery := mdns.NewMdnsService(node, discoveryName, &discoveryNotifee{node: node})
+	_ = discovery.Start()
+	t.Cleanup(func() {
+		node.Close()
+		discovery.Close()
+	})
+	return node
+}
+
+func TestDiscoveryHandlePeerFound(t *testing.T) {
+	host, _ := libp2p.New()
+	discovery := mdns.NewMdnsService(host, "discoveryRoomTest", &discoveryNotifee{node: host})
+	_ = discovery.Start()
+
+	t.Cleanup(func() {
+		host.Close()
+		discovery.Close()
+	})
+
+	secondPeer := secondPeer(t, "discoveryRoomTest")
+	limit := time.Now()
+
+	//wait till the other Peer is found, limit 4 seconds
+	for {
+		if time.Now().After(limit.Add(4 * time.Second)) {
+			break
+		}
+		if len(PeerList) == 0 {
+			continue
+		}
+		break
+	}
+
+	assert.Contains(t, host.Peerstore().Peers(), secondPeer.ID())
+}
